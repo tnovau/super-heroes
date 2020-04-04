cc713243b3b8c271c4648b6cb6c7514cbb1d7b8e
Tomás Novau <tnovau@gmail.com>
2020-04-04 19:24:41 +0200

    Adding setupProxy for local development.

--- src/index.js
+++ src/index.js
@@ -11,6 +11,17 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+fetch('/api/marvel/characters', {
+  method: 'GET'
+})
+.then(res => res.json())
+.then(data => {
+  console.log(data);
+})
+.catch(err => {
+  console.log(err);
+});
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
