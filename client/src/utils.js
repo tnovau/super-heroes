9258be47a89864736f9308150f11272558e8cac1
Tomás Novau <tnovau@gmail.com>
2020-04-16 00:10:40 +0200

    Making cleaner the isIe function

--- client/src/utils.js
+++ client/src/utils.js
@@ -1,1 +1,7 @@
-export const isIE = (userAgent) => userAgent.indexOf("MSIE ") > -1 || userAgent.indexOf("Trident/") > -1;
+/** @param {string} userAgent */
+const isIe10orOlder = (userAgent) => userAgent.indexOf("MSIE ") > -1;
+/** @param {string} userAgent */
+const isIe11orNewer = (userAgent) => userAgent.indexOf("Trident/") > -1;
+
+/** @param {string} userAgent */
+export const isIE = (userAgent) => isIe10orOlder(userAgent) || isIe11orNewer(userAgent);
