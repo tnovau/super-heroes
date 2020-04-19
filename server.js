4a84298c058bc0f346b0d98910c0d0f3c425595d
Tomás Novau <tnovau@gmail.com>
2020-04-19 18:13:22 +0200

    Handles any requests that don't match with /api/marvel

--- server.js
+++ server.js
@@ -27,6 +27,10 @@ app.use(
   })
 );
 
+app.get('*', (_req, res) => {
+  res.sendFile(clientBuildPath, 'index.html');
+});
+
 app.listen(port, () => {
   console.log(`App running in port: ${port}`);
 });
